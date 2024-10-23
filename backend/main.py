1dae88db81947f18453846c27072b6ebadb15808
tushar242001 <tusharadling24@gmail.com>
2024-10-23 17:34:42 +0530

    Project Updated

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template,redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash,check_password_hash
 import pymysql
+from sqlalchemy import text
 pymysql.install_as_MySQLdb()
 # mydatabase connection 
 local_server=True
@@ -12,22 +14,61 @@ app.secret_key="TusharAdling"
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:@localhost/covid'
 db=SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    srfid = db.Column(db.String(200), unique=True, nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    phone = db.Column(db.Integer)
 
-class Test(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(50))
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/usersignup")
+@app.route('/usersignup',methods=['POST','GET'])
 def usersignup():
+    if request.method=="POST":
+        srfid=request.form.get('srf')
+        email=request.form.get('email')
+        password=request.form.get('password')
+        phone=request.form.get('phone')
+        encpassword=generate_password_hash(password,method='pbkdf2:sha256')
+        # print(srfid,email,phone,password)
+        new_user = db.session.execute(
+            text("INSERT INTO `user` (`srfid`, `email`, `password`, `phone`) VALUES (:srfid, :email, :password, :phone)"),
+            {"srfid": srfid, "email": email, "password": encpassword, "phone": phone}
+        )
+        db.session.commit()  # Commit the transaction
+        return 'User Added'
     return render_template("usersignup.html")
 
-@app.route("/userlogin")
+
+@app.route('/userlogin',methods=['POST','GET'])
 def userlogin():
+    if request.method=="POST":
+        srfid=request.form.get('srf')
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=User.query.filter_by(srfid=srfid).first()
+
+        if user and check_password_hash(user.password,password):
+            return 'Login Success'
+        else:
+            return 'login failed'
     return render_template("userlogin.html")
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route("/hospitalsignup")
 def hospitalsignup():
@@ -46,19 +87,5 @@ def adminsignup():
     return render_template("adminsignup.html")
 
 
-
-
-
-# testing wherther db is connected or not
-@app.route("/test")
-def test():
-    try:
-        a=Test.query.all()
-        print(a)
-        return 'MY DATABASE IS CONNECTED'
-    
-    except Exception as e:
-        return f'MY DATABASE IS NOT CONNECTED {e}'
-    
 
 app.run(debug=True)
